3178687277ca5f8bbe02adc4af09a24763adcfab
Surya Teja <94suryateja@gmail.com>
2017-12-09 16:14:11 +0530

    encryption

--- app.py
+++ app.py
@@ -4,11 +4,38 @@ from sys import argv
 import pyperclip
 # from pathlib import Path
 import os
+import base64
 import json
 import pyperclip
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-FILE = os.getcwd()+"\\store"
-print(FILE)
+FILE = "store"
+PASSWORD = b"This"
+SALT = b'3\x97\x16\x1b\x8e\x92\xef\xaf\x06\xaeT\xdcL\xad\xe4\x11'
+
+class Crypt:
+    def __init__(self):
+        self.kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=SALT,
+            iterations=100000,
+            backend=default_backend()
+        )
+        self.key = base64.urlsafe_b64encode(self.kdf.derive(PASSWORD))
+        self.f = Fernet(self.key)
+
+    def encrypt(self, data):
+        encrypted_data = self.f.encrypt(bytes(data,'utf-8'))
+        print(encrypted_data)
+        return encrypted_data
+
+    def decrypt(self, encrypted_data):
+        return self.f.decrypt(encrypted_data)
+
 
 def addCred():
     if not os.path.isfile(FILE):
@@ -25,7 +52,7 @@ def addCred():
                 if len(input_host) != 2:
                     print("Invalid input. exit the program")
                     quit()
-                cred_obj[input_host[0]] = input_host[1]
+                cred_obj[input_host[0]] = Crypt.encrypt(input_host[1])
 
         with open(FILE,'w') as f:
             f.write(json.dumps(cred_obj))
@@ -52,7 +79,7 @@ def addCred():
                 if len(input_host) != 2:
                     print("Invalid input. exit the program")
                     quit()
-                data[input_host[0]] = input_host[1]
+                data[input_host[0]] = Crypt.encrypt(input_host[1])
 
         with open(FILE,'w') as f:
             f.write(json.dumps(data))
@@ -60,8 +87,7 @@ def addCred():
 
 def getCred():
     if not os.path.isfile(FILE):
-        print("It seems file doesn't exist. Please start program\
-            using 'new' argument add a credential.")
+        print("It seems file doesn't exist. Please start program using 'new' argument add a credential.")
     else:
         with open(FILE,'r') as f:
             data = f.read()
@@ -85,10 +111,13 @@ def getCred():
             else:
                 #copy to clipboard.
                 result = data_dict[name]
-                pyperclip.copy(result)
+                pyperclip.copy(Crypt.decrypt(result).decode('UTF-8'))
                 print("Password copied to clipboard")
 
 if __name__ == '__main__':
+
+    #initialise class
+    crypting = Crypt()
     if len(argv) > 1:
         if argv[1] == "new":
             addCred()
